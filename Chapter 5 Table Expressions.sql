fdf8c0ebdadb784dacdf64b2fd733ef253baf25a
HyunKoun Kim(Tony) <53275791+Tony-Kim09@users.noreply.github.com>
2020-01-07 01:24:09 -0500

    Update Chapter 5 Table Expressions.sql

--- Chapter 5 Table Expressions.sql
+++ Chapter 5 Table Expressions.sql
@@ -19,7 +19,7 @@ Invalid column name 'endofyear'.
 */
 -- Explain what the problem is and suggest a valid solution.
 
-/* SQL Server processes a query in this order: FROM, WHERE, GROUP BY, HAVING, SELECT, and ORDER BY.
+/* Answer: SQL Server processes a query in this order: FROM, WHERE, GROUP BY, HAVING, SELECT, and ORDER BY.
    Since WHERE clause is processed before the SELECT clause, SQL Server will not know any aliases 
    created in the SELECT clause. Therefore if we reference a column based on the alias, SQL Server
    will not know what it is referring to. */
@@ -41,8 +41,8 @@ FROM Sales.Orders
 GROUP BY empid;
 
 --Question 2.2: Encapsulate the query from exercise 2.1 in a derived table
---				Write a join query between the derived table and the Sales.Orders
---				table to return the Sales.Orders with the maximum order date for each employee
+--		Write a join query between the derived table and the Sales.Orders
+--		table to return the Sales.Orders with the maximum order date for each employee
 
 SELECT T.empid, T.maxorderdate, O.orderid, O.custid
 FROM Sales.Orders AS O
@@ -54,27 +54,27 @@ INNER JOIN
 		AND O.orderdate = T.maxorderdate;
 
 --Question 3.1: Write a query that calculates a row number for each order
---				based on orderdate, orderid ordering
+--		based on orderdate, orderid ordering
 
 SELECT orderid, orderdate, custid, empid,
 		ROW_NUMBER() OVER(ORDER BY orderdate, orderid) AS rownum
 FROM Sales.Orders;
 
 --Question 3.2: Write a query that returns rows with row numbers 11 through 20
---				based on the row number definition in exercise 3.1
---				Use a CTE to encapsulate the code from exercise 3.1
+--		based on the row number definition in exercise 3.1
+--		Use a CTE to encapsulate the code from exercise 3.1
 
 WITH CTE AS 
-		(SELECT orderid, orderdate, custid, empid,
-			ROW_NUMBER() OVER(ORDER BY orderdate, orderid) AS rownum
-		 FROM Sales.Orders)
+	(SELECT orderid, orderdate, custid, empid,
+		ROW_NUMBER() OVER(ORDER BY orderdate, orderid) AS rownum
+	 FROM Sales.Orders)
 
 SELECT orderid, orderdate, custid, empid, rownum
 FROM CTE
 WHERE rownum BETWEEN 11 and 20;
 
 --Question 4: Write a solution using a recursive CTE that returns the management
---			  chain leading to Patricia Doyle (employee ID 9)
+--	      chain leading to Patricia Doyle (employee ID 9)
 
 WITH mghier AS
 	(SELECT empid, mgrid, firstname, lastname
@@ -105,7 +105,7 @@ CREATE VIEW vEmpOrd AS
 GO
 
 --Question 5.2 (Advanced): Write a query against Sales.vEmpOrders that returns
---						   the running qty for each employee and year
+--			   the running qty for each employee and year
 
 SELECT v.empid, v.orderyear, v.ttlqty, 
 				(SELECT SUM(ttlqty)
@@ -116,9 +116,9 @@ FROM Sales.vEmpOrd AS v
 ORDER BY v.empid, v.orderyear;
 
 --Question 6.1: Create an inline function that accepts as inputs a supplier id
---				(@supid AS INT), and a requested number of products (@n AS INT)
---				The function should return @n products with the highest unit prices
---				that are supplied by the given supplier id
+--		(@supid AS INT), and a requested number of products (@n AS INT)
+--		The function should return @n products with the highest unit prices
+--		that are supplied by the given supplier id
 
 DROP FUNCTION IF EXISTS Production.TopProducts;
 GO
@@ -134,8 +134,8 @@ RETURN
 	ORDER BY unitprice DESC;
 GO
 --Question 6.2: Using the CROSS APPLY operator and the function you created
---				in exercise 6.1, return, for each supplier, the two most
---				expensive products
+--		in exercise 6.1, return, for each supplier, the two most
+--		expensive products
 
 SELECT S.supplierid, S.companyname, T.productid, T.productname, T.unitprice
 FROM Production.Suppliers AS S
