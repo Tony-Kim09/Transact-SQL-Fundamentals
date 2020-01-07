264c1753a6dcb49a3304a85b121a86a84b473ba8
HyunKoun Kim(Tony) <53275791+Tony-Kim09@users.noreply.github.com>
2020-01-07 01:39:15 -0500

    Update Chapter 6 Set Operators.sql

--- Chapter 6 Set Operators.sql
+++ Chapter 6 Set Operators.sql
@@ -10,13 +10,13 @@
    UNION ALL keeps them. */
 
 --Question 2: Write a query that generates a virtual auxiliary table of 10 numbers
---			  in the range 1 through 10
+--	      in the range 1 through 10
 
 SELECT n
 FROM (VALUES(1),(2),(3),(4),(5),(6),(7),(8),(9),(10)) AS Num(n);
 
 --Question 3: Write a query that returns customer and employee pairs
---			  that had order activity in January 2016 but not in February 2016
+--	      that had order activity in January 2016 but not in February 2016
 
 SELECT custid, empid
 FROM Sales.Orders
@@ -29,7 +29,7 @@ FROM Sales.Orders
 WHERE orderdate >= '20160201' AND orderdate < '20160301';
 
 --Question 4: Write a query that returns customer and employee pairs
---			  that had order activity in both January 2016 and February 2016
+--	      that had order activity in both January 2016 and February 2016
 
 SELECT custid, empid
 FROM Sales.Orders
@@ -42,8 +42,8 @@ FROM Sales.Orders
 WHERE orderdate >= '20160201' AND orderdate < '20160301';
 
 --Question 5: Write a query that returns customer and employee pairs
---			  that had order activity in both January 2016 and February 2016
---			  but not in 2015
+--	      that had order activity in both January 2016 and February 2016
+--	      but not in 2015
 
 (SELECT custid, empid
 FROM Sales.Orders
@@ -79,10 +79,10 @@ FROM Production.Suppliers;
 
 SELECT country, region, city
 FROM (SELECT 1 AS sorted, country, region, city
-		FROM HR.Employees
+      FROM HR.Employees
 
-		UNION ALL
+      UNION ALL
 	
-	  SELECT 2, country, region, city
-	    FROM Production.Suppliers) AS sortedNew
+      SELECT 2, country, region, city
+      FROM Production.Suppliers) AS sortedNew
 ORDER BY sorted, country, region, city;
